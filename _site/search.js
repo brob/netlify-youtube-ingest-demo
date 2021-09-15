24c9c5497bb70f43f6f653c3a73fb74ea9bccd6a
Bryan Robinson <bryanlrobinson@gmail.com>
2021-09-15 16:05:22 -0400

    Fixes object id var

--- _site/search.js
+++ _site/search.js
@@ -10,7 +10,7 @@ search.addWidgets([
         item: `
                 <div>
                   <h2><a href="https://youtube.com/watch?v={{objectID}}">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</a></h2>
-                  <div class="video-container"><iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/{{objectID}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                  <div class="video-container"><iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/{{ objectID }}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                 </div>
             `
 
